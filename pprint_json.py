10e0f26186127a00c158de9884d71d32664be6ea
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-03-31 18:55:45 +0300

    Update pprint_json.py
    Исправлен код при запуске скрипта. Параметр с файлом передается в строке запуска.
    Добавлена обработка исключений, если файл не найден.

--- pprint_json.py
+++ pprint_json.py
@@ -7,13 +7,16 @@ def load_data(path):
         return json.load(file)
 
 
-def pretty_print_json(json_loading_data):
+def pretty_json(json_loading_data):
     return json.dumps(json_loading_data, ensure_ascii=False, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        file_path = sys.argv[1]
+    if len(sys.argv) == 1:
+        print("No file or directory")
     else:
-        file_path = input("Please, input correct file path: ")
-    print(pretty_print_json(load_data(file_path)))
+        try:
+            print(pretty_json(load_data(sys.argv[1])))
+        except (IOError, Exception) as e:
+            print(e)
+
