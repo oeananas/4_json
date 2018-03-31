893cc4eb7b48d97966fdabb9b7d2a519ad1d67c2
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-03-31 20:14:24 +0300

    Update pprint_json.py
    Исправлено название функции, исправлена обработка исключений и поведение программы

--- pprint_json.py
+++ pprint_json.py
@@ -7,16 +7,14 @@ def load_data(path):
         return json.load(file)
 
 
-def pretty_json(json_content):
+def pretty_view_json(json_content):
     return json.dumps(json_content, ensure_ascii=False, indent=4, sort_keys=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("No file or directory")
-    else:
-        try:
-            print(pretty_json(load_data(sys.argv[1])))
-        except (IOError, Exception) as e:
-            print(e)
-
+        exit("Need input file path as parameter")
+    try:
+        print(pretty_view_json(load_data(sys.argv[1])))
+    except (FileNotFoundError) as e:
+        print("File not found")
