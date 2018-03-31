8e0c419910d710046fc746e44ea05fa9b46f06ac
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-03-31 11:17:25 +0300

    Update pprint_json.py
    Добавлены функции, работающие с файлом .json
    Путь до файла указывается в строке вызова скрипта, либо сразу после запуска скрипта

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,19 @@
 import json
+import sys
 
 
-def load_data(filepath):
-    pass
+def load_data(path):
+    with open(path, "r") as file:
+        return json.load(file)
 
 
 def pretty_print_json(data):
-    pass
+    return json.dumps(data, indent=4, sort_keys=True)
 
 
 if __name__ == '__main__':
-    pass
+    if ".json" in sys.argv[1]:
+        file_path = sys.argv[1]
+    else:
+        file_path = input("Please, input correct file path: ")
+    print(pretty_print_json(load_data(file_path)))
